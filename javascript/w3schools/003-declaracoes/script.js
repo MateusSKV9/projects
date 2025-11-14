0e5410e82508d9c2333b85161243c7fdf793d9b7
Mateus Santos <mateussilvanzg@gmail.com>
2025-11-13 21:42:00 -0300

    feat(javascript): start new course

---
+++ javascript/w3schools/003-declaracoes/script.js
@@ -0,0 +1,11 @@
+// Comandos e Instruções
+let x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+console.log(z);
+
+function myFunction() {
+	const div = document.getElementById("demo");
+	div.innerText = "Olá, Mundo!";
+}
